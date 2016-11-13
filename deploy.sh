755b202e881a792b853e621c7f9e6736a679398a
Thor Kell <thor@tide-pool.ca>
2016-11-12 19:22:23 -0500

    Fixing deprecations and deploys

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 echo "beginning rsync"
 rsync -r --exclude 'deploy.sh' --exclude '.git' \
-/home/thor/Code/beethoven/* tidepool@tide-pool.ca:/home/tidepool/www/beethoven
+/Users/thor/Code/beethoven/* tidepool@tide-pool.ca:/home/tidepool/www/beethoven
 echo "rsync complete!"
